0ed2d2dd8ceb021adc3bddaf86568f9e8d5bbc8d
Eric Raio <ericraio@gmail.com>
2016-03-20 16:55:58 -0700

    updating master with latest schedules

--- Rakefile.rb
+++ Rakefile.rb
@@ -10,5 +10,8 @@ end
 namespace :deploy do
   task :schedule do
     deploy :schedule
+    system "git add schedule.txt"
+    system "git commit -m 'setting schedule for next post'"
+    system "git push origin master"
   end
 end
