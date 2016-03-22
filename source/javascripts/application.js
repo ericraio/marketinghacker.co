4a9f74e009668a8b5878770c6bdd3354257a4369
Eric Raio <ericraio@gmail.com>
2016-03-21 21:32:00 -0700

    adding search!

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -3,7 +3,8 @@
 //= require fullpage.js/dist/jquery.fullpage
 //= require selection-sharer/dist/selection-sharer
 //= require vendor/casper
-//= require lunr.min
+//= require lunr.js/lunr
+//= require handlebars/handlebars
 
 
 jQuery(function($) {
@@ -18,4 +19,70 @@ jQuery(function($) {
   });
 
   $('.post-content').selectionSharer();
+
+  function populateIndex(data) {
+    var index = lunr(function(){
+      this.field('title', { boost: 10 });
+      this.field('content');
+      this.ref('id');
+    });
+    data.forEach(function(item) {
+      index.add(item);
+    });
+    return index;
+  }
+
+  function contentList(data) {
+    var contents = [];
+    data.forEach(function(item) {
+      contents.push(item);
+    });
+    return contents;
+  }
+
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+
+  function searchSetup(index, contents){
+    // Set up Handlebars template
+    var resultsTemplate = Handlebars.compile($("#results-template").html());
+
+    // various event handlers...
+
+    $("#search-field").bind("keyup", debounce(function(){
+      $(".search-results").empty();
+      if ($(this).val() < 2) return;
+      var query = $(this).val();
+      var results = index.search(query).slice(0,4);
+      $.each(results, function(index, result){
+        $(".search-results").append(resultsTemplate({
+          title: contents[result.ref].title,
+          url: contents[result.ref].url
+        }));
+      });
+    }, 50, true));
+  }
+
+  $.getJSON("/search.json", function(data) {
+    var index = populateIndex(data);
+    var contents = contentList(data);
+    searchSetup(index, contents);
+    // other code for app below - anything that needs search functionality
+    // lives inside of this callback
+    // ...
+  });
+
+
 });
